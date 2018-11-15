03cb977bc75761a8e1471e4f627fe5fdcaceede6
Yan Paulo <yanpaulo@hotmail.com>
2018-11-15 17:55:30 -0300

    Migration automático.

--- AspNetDemo/Startup.cs
+++ AspNetDemo/Startup.cs
@@ -95,6 +95,21 @@ namespace AspNetDemo
             app.UseAuthentication();
 
             app.UseMvc();
+
+            UpdateDatabase(app);
+        }
+
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
         }
     }
 }
